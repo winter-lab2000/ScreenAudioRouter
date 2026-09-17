4a4e3316584f63be2ee4a62a82499d901307e1e9
winter <51295923+yuntianDong@users.noreply.github.com>
2026-09-18 00:19:25 +0800

    feat: add src/ScreenAudioRouter/GlobalUsings.cs

---
+++ src/ScreenAudioRouter/GlobalUsings.cs
@@ -0,0 +1,17 @@
+global using System;
+global using System.Collections.Generic;
+global using System.IO;
+global using System.Linq;
+global using Application = System.Windows.Application;
+global using MessageBox = System.Windows.MessageBox;
+global using Color = System.Windows.Media.Color;
+global using Colors = System.Windows.Media.Colors;
+global using Brush = System.Windows.Media.Brush;
+global using Brushes = System.Windows.Media.Brushes;
+global using SolidColorBrush = System.Windows.Media.SolidColorBrush;
+global using FontFamily = System.Windows.Media.FontFamily;
+global using Point = System.Windows.Point;
+global using Cursors = System.Windows.Input.Cursors;
+global using Button = System.Windows.Controls.Button;
+global using TextBox = System.Windows.Controls.TextBox;
+global using ComboBox = System.Windows.Controls.ComboBox;
